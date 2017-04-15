e449ee0463b942bcbda9e687f6ed8d32964fc0fe
Zach Ward <zach.l.ward6@gmail.com>
2017-04-15 16:56:56 -0400

    npm init, add necessary js libraries, configure webpack

--- Gemfile
+++ Gemfile
@@ -22,7 +22,13 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+
+gem 'annotate'
+gem 'pry-rails'
+gem 'better_errors'
+gem 'binding_of_caller'
+
 
 # Use Unicorn as the app server
 # gem 'unicorn'
@@ -42,4 +48,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/zacmo.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  devtool: 'source-map'
+};

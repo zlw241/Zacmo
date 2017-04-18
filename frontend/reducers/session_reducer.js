b1d3325aed152a2cea9840dff2055e021fd0bddb
Zach Ward <zach.l.ward6@gmail.com>
2017-04-18 16:02:19 -0400

    define SessionReducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,27 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const _nullUser = Object.freeze({
+  currentUser: null,
+  errors: []
+});
+
+const SessionReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER: {
+      const newState = merge({}, state);
+      newState.currentUser = action.currentUser;
+      return newState;
+    }
+    case RECEIVE_ERRORS: {
+      const newState = merge({}, state);
+      newState.errors = actions.errors;
+      return newState;
+    }
+    default:
+      return state
+  }
+};
+
+export default SessionReducer;

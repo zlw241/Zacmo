f2a3d541225bf3af8ee2950a632b5731ae904254
Zach Ward <zach.l.ward6@gmail.com>
2017-04-18 17:31:41 -0400

    create App component, add session form folder and component files

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = ({children}) => {
+  return (
+    <h1>Zacmo</h1>
+  );
+}
+
+export default App;
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from './app.jsx';
 
 const Root = ({store}) => {
+
+  const _ensureLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (!currentUser) {
+      replace('/login');
+    }
+  };
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  };
+
+
   return (
-    <h1>Zacmo</h1>
-  )
+    <Provider store={store}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
+    </Provider>
+  );
 };
 
 export default Root;

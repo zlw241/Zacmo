0a0cf48d3e3bfbd75aaa47a5ab393892feed60f6
Zach Ward <zach.l.ward6@gmail.com>
2017-04-18 13:05:38 -0400

    api methods for create, show, update, delete

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,12 +10,18 @@ class Api::UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find(params[:id])
+    render :show
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :phone_num, :first_name, :last_name)
   end
 end

baab14ac6108f47ad00472426b58ad522893dc47
Zach Ward <zach.l.ward6@gmail.com>
2017-04-18 12:22:59 -0400

    rails generated files from changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :users, only: [:create, :show, :update, :destroy]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

2259ee78f3da07a73338fbe163374bf3a37af732
Zach Ward <zach.l.ward6@gmail.com>
2017-04-19 19:21:39 -0400

    live reload for css

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -6,18 +6,19 @@ import { hashHistory } from 'react-router';
 class Profile extends React.Component {
   constructor(props) {
     super(props);
+    this.state = this.props.user;
   }
 
   componentWillMount() {
     this.props.fetchUser(this.props.currentUser.id);
   }
 
+
   render() {
   //  if (!this.props.currentUser) { return null }
-
     return (
       <div className="profile">
-        <h1 id="red">{this.props.user.first_name} {this.props.user.last_name}</h1>
+        <h1>{this.props.user.first_name} {this.props.user.last_name}</h1>
         <div>{this.props.user.username}</div>
         <div>{this.props.user.email}</div>
         <Link to="/account/settings">Edit</Link>
--- frontend/components/user/user.jsx
+++ frontend/components/user/user.jsx
@@ -6,14 +6,24 @@ class User extends React.Component {
     super(props)
   }
 
-  componentDidMount() {
-    this.props.fetchUser(this.props.params.user_id);
+  componentWillMount() {
+    this.props.fetchUser(parseInt(this.props.params.user_id));
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.params.user_id !== nextProps.params.user_id) {
+      this.props.fetchUser(nextProps.params.user_id);
+    }
   }
 
   render() {
 
     return (
       <div className="user">
+        <Link to="/1">1</Link>
+        <Link to="/5">5</Link>
+        <Link to="/12">12</Link>
+        <Link to="/13">13</Link>
         <h1 id="red">{this.props.user.first_name} {this.props.user.last_name}</h1>
         <div>{this.props.user.username}</div>
         <div>{this.props.user.email}</div>

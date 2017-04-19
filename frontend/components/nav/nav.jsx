f4c5bbc942008adc45de924a12437f0a69720b19
Zach Ward <zach.l.ward6@gmail.com>
2017-04-18 22:52:41 -0400

    nest divs properly, add some formatting

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -8,22 +8,30 @@ class Nav extends React.Component {
 
   render() {
 
+    let navItems = (
+      <div className="nav">
+        <Link to='/login'>Log In</Link>
+        <Link to='/signup'>Sign Up</Link>
+      </div>
+    );
+
+
     if (this.props.currentUser) {
-      return (
-        <div>
-          <p>Hello! {this.props.currentUser.username}</p>
+      const username = this.props.currentUser.username;
+      navItems = (
+        <div className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/">{username}</Link>
           <button onClick={this.props.logout}>Logout</button>
         </div>
       );
-    } else {
-      return (
-        <nav>
-          <Link to='/login'>Log In</Link>
-          <br />
-          <Link to='/signup'>Sign Up</Link>
-        </nav>
-      );
     }
+
+    return (
+      <nav>
+        {navItems}
+      </nav>
+    );
   }
 };
 

dfa9ae113c922392526efbb90307ff7deeca30b6
Zach Ward <zach.l.ward6@gmail.com>
2017-04-18 22:24:18 -0400

    logout session action passes null value to receive_user

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -29,7 +29,7 @@ export const login = (user) => (dispatch) => {
 
 export const logout = () => (dispatch) => {
   return APIUtil.logout().then(
-    (loggedOutUser) => dispatch(receiveUser(loggedOutUser)),
+    () => dispatch(receiveUser(null)),
     (err) => dispatch(receiveErrors(err.responseJSON))
   );
 };

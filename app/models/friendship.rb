47331e323b2f424cbdb28636e890bfd6e475f077
Zach Ward <zach.l.ward6@gmail.com>
2017-04-20 16:52:08 -0400

    add status column to friendship table

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,7 +1,7 @@
 class Friendship < ActiveRecord::Base
-
   validates :user_id, :friend_id, presence: true
-
+  validates :status, inclusion: { in: ["pending", "requested", "accepted") }
+    
   belongs_to :friend,
     class_name: "User",
     foreign_key: :friend_id
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420204012) do
+ActiveRecord::Schema.define(version: 20170420204607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170420204012) do
     t.integer  "friend_id",  null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
   add_index "friendships", ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true, using: :btree

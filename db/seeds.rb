da5525595470890d19eaee8cce095877bc1f8ba3
Zach Ward <zach.l.ward6@gmail.com>
2017-04-21 12:50:04 -0400

    fix second error in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.destroy_all!
+User.destroy_all
 
 users = User.create([
     {

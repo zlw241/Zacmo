76b5f52f0ba2cda0f906c3c13a73966aa0ecd2eb
Zach Ward <zach.l.ward6@gmail.com>
2017-04-21 16:03:03 -0400

    Create feed component

---
+++ frontend/components/feed/feed.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="feed">Feed</div>
+    );
+  }
+}
+
+export default Feed;

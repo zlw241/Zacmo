1c5a33a38c91b2892723e8321d21a693ad405f8e
Zach Ward <zach.l.ward6@gmail.com>
2017-04-21 16:03:44 -0400

    Create notifications component

---
+++ frontend/components/notifications/notifications.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div id="notifications">Notifications</div>
+    );
+  }
+}
+
+export default Notifications;
---
+++ frontend/components/notifications/notifications_container.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Notifications from './notifications';
+
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Notifications);

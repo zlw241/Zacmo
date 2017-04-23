49586a5f5996eff835127cf13d53f127ec0130cd
Zach Ward <zach.l.ward6@gmail.com>
2017-04-23 19:02:51 -0400

    add show, create, index methods for transactions

---
+++ app/controllers/api/transactions_controller.rb
@@ -0,0 +1,33 @@
+class Api::TransactionsController < ApplicationController
+  def create
+    @transaction = current_user.transactions.new(transaction_params)
+    if @transaction.save
+      render "/api/transactions/show"
+    else
+      render json: @transaction.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+  end
+
+  def index
+    friends = current_user.friends.pluck(:id)
+    @transactions = Transaction
+      .where("transactions.user_id IN (:friend_ids) OR transactions.recipient_id IN (:friend_ids) OR transactions.user_id = :id OR transactions.recipient_id = :id", friend_ids: friends, id: current_user.id)
+      .order("created_at DESC")
+      .all
+
+    puts @transactions
+    render "/api/transactions/index"
+  end
+
+  def show
+    @transaction = Transaction.find_by(id: params[:transaction_id])
+    render "/api/transactions/show"
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:recipient_id, :memo, :amount)
+  end
+end

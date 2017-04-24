0abb83a92c874fd0e74934439b5a04415f737e01
Zach Ward <zach.l.ward6@gmail.com>
2017-04-24 17:37:08 -0400

    new schema generated by migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423025134) do
+ActiveRecord::Schema.define(version: 20170424204646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,8 @@ ActiveRecord::Schema.define(version: 20170423025134) do
     t.datetime "updated_at",                      null: false
   end
 
-  add_index "transactions", ["user_id", "recipient_id"], name: "index_transactions_on_user_id_and_recipient_id", unique: true, using: :btree
+  add_index "transactions", ["recipient_id"], name: "index_transactions_on_recipient_id", using: :btree
+  add_index "transactions", ["user_id"], name: "index_transactions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",                      null: false

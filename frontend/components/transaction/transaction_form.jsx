1974bf9e17e298f81383062419e3180eca89e2aa
Zach Ward <zach.l.ward6@gmail.com>
2017-04-24 17:35:07 -0400

    basic functionality for paying people

--- frontend/components/transaction/transaction_form.jsx
+++ frontend/components/transaction/transaction_form.jsx
@@ -8,15 +8,24 @@ class TransactionForm extends React.Component {
     this.state = {
       recipient: "",
       amount: "",
-      memo: ""
+      memo: "",
+      showForm: {display: 'none'}
     }
 
     this.checkTransactionValidity = this.checkTransactionValidity.bind(this)
     this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.showForm = this.showForm.bind(this);
   }
 
   checkTransactionValidity() {
     // console.log(this.state.amount.split(" ")[0].splice(1))
+  }
+
+  showForm() {
+    this.setState({
+      showForm: {}
+    });
   }
 
   handleInput(e) {
@@ -25,6 +34,25 @@ class TransactionForm extends React.Component {
       [field]: e.currentTarget.value
     });
     console.log(this.state);
+  }
+
+  clearForm() {
+    this.setState({
+      recipient: "",
+      amount: "",
+      memo: "",
+      showForm: {display: 'none'}
+    });
+  }
+
+  handleSubmit(e) {
+    const recipient_username = this.state.recipient;
+    const amount = this.state.amount;
+    const memo = this.state.memo;
+
+    const newTransaction = {memo, amount, recipient_username }
+    this.props.createTransaction(newTransaction)
+    this.clearForm();
   }
 
   render() {
@@ -41,7 +69,8 @@ class TransactionForm extends React.Component {
                   onChange={this.handleInput}
                   placeholder="Amount"
                   type="search"
-                  value={this.state.amount} />
+                  value={this.state.amount}
+                  onFocus={this.showForm} />
               </div>
               <div className="transaction-recipient">
                 <input
@@ -49,13 +78,16 @@ class TransactionForm extends React.Component {
                   placeholder="Pay or charge someone"
                   type="search"
                   value={this.state.recipient}
-                  onChange={this.handleInput} />
-
+                  onChange={this.handleInput}
+                  onFocus={this.showForm} />
               </div>
             </div>
-            <div className="memo">
-              <textarea id="transaction-memo"></textarea>
-              <div className="memo-bottom">
+            <div className="memo" style={this.state.showForm}>
+              <textarea name="memo" id="transaction-memo"
+                placeholder="for ice-cream!"
+                value={this.state.memo}
+                onChange={this.handleInput}></textarea>
+              <div className="memo-bottom hidden">
                 <div className="select-visibility">
                   <select>
                     <option>Public</option>
@@ -63,7 +95,7 @@ class TransactionForm extends React.Component {
                     <option>Private</option>
                   </select>
                 </div>
-                <div className="submit-transaction">Pay</div>
+                <div onClick={this.handleSubmit} className="submit-transaction">Pay</div>
               </div>
             </div>
           </div>
--- frontend/components/transaction/transaction_form_container.jsx
+++ frontend/components/transaction/transaction_form_container.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createTransaction } from '../../actions/transaction_actions';
+
 import TransactionForm from './transaction_form';
 
 
 const mapStateToProps = (state) => ({
 
-})
+});
 
 const mapDispatchToProps = (dispatch) => ({
+  createTransaction: (transaction) => dispatch(createTransaction(transaction))
+});
 
-})
 
 export default connect(mapStateToProps, mapDispatchToProps)(TransactionForm);

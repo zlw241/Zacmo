7e7ca99bfdfbce36e427a258fa30e98f918a1a62
Zach Ward <zach.l.ward6@gmail.com>
2017-04-24 21:28:41 -0400

    routes add for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
 
 
-
   namespace :api, defaults: { format: :json } do
+
     resources :users, only: [:create, :show, :update, :destroy, :index] do
       get "search", on: :collection
       resources :friendships, only: [:create, :destroy]
     end
+
     resource :session, only: [:create, :destroy]
+
     resources :friendships, only: [:destroy, :update]
 
     resources :transactions, only: [:create, :show, :index, :destroy]
+
+    resources :comments, only: [:create, :destroy]
+
   end
 
   root "static_pages#root"

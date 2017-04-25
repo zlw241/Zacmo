61e2d1acd32c6190f7eabeb0f6c0aa0a33bd28a3
Zach Ward <zach.l.ward6@gmail.com>
2017-04-24 23:57:23 -0400

    add heart icon to like posts, show number of likes for post

--- frontend/components/transaction/transaction_detail.jsx
+++ frontend/components/transaction/transaction_detail.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 import { connect } from 'react-redux';
-import { createComment } from '../../actions/transaction_actions';
+import { createComment, addLike } from '../../actions/transaction_actions';
 import CommentList from '../comment/comment_list';
-
+import Likes from '../like/likes.jsx';
 
 class TransactionDetail extends React.Component {
   constructor(props) {
@@ -11,18 +11,20 @@ class TransactionDetail extends React.Component {
     this.state = { body: "" }
     this.addComment = this.addComment.bind(this);
     this.handleInput = this.handleInput.bind(this);
+    this.like = this.like.bind(this);
+  }
+
+  like() {
+    this.props.addLike(this.props.transaction.id)
   }
 
   addComment(e) {
     e.preventDefault()
     const newComment = Object.assign({}, this.state)
-    newComment.transaction_id = this.props.transaction.id
-    this.props.createComment(newComment).then(
+    const transactionId = this.props.transaction.id
+    this.props.createComment(transactionId, newComment).then(
       () => this.setState({body: ""})
     )
-    // this.setState({
-    //   body: ""
-    // });
   }
 
   handleInput(e) {
@@ -51,6 +53,20 @@ class TransactionDetail extends React.Component {
       }
     }
 
+    let like = "likes";
+    if (this.props.transaction.num_likes === 1) {
+      like = "like";
+    }
+
+    let likeButton = (
+      <i className="fa fa-heart-o" aria-hidden="true"></i>
+    )
+    if (this.props.transaction.liked_by_user === true) {
+      likeButton = (
+        <i className="fa fa-heart red" aria-hidden="true"></i>
+      )
+    }
+
     return (
       <div className="transaction-detail">
         <div className="transaction-header">
@@ -62,8 +78,17 @@ class TransactionDetail extends React.Component {
 
           <span className="transaction-amount">{transactionAmount}</span>
         </div>
-        <div className="likes">[likes]</div>
         <div className="transaction-memo">{this.props.transaction.memo}</div>
+        <div className="likes">
+
+          <div onClick={this.like} className="like-button">
+            {likeButton}
+          </div>
+
+          <div className="likes-count">
+            {this.props.transaction.num_likes} {like}
+          </div>
+        </div>
         <div className="comments">
           <CommentList comments={this.props.transaction.comments} />
         </div>
@@ -77,62 +102,14 @@ class TransactionDetail extends React.Component {
         </div>
       </div>
     );
-
   }
 }
 
 
-
 const mapDispatchToProps = (dispatch) => ({
-  createComment: (comment) => dispatch(createComment(comment))
+  createComment: (transactionId, comment) => dispatch(createComment(transactionId, comment)),
+  addLike: (like) => dispatch(addLike(like))
 });
 
 
 export default connect(null, mapDispatchToProps)(TransactionDetail);
-
-
-// const TransactionDetail = ({transaction, currentUser, addComment}) => {
-//
-//   if (!currentUser) { return null }
-//
-//   let transactionAmount = null;
-//   if (transaction.amount) {
-//     if (transaction.recipient.id === currentUser.id) {
-//       transactionAmount = (
-//         <span className="transaction-amount green">
-//           +${transaction.amount}
-//         </span>
-//       );
-//     } else {
-//       transactionAmount = (
-//         <span className="transaction-amount red">
-//           -${transaction.amount}
-//         </span>
-//       );
-//     }
-//   }
-//
-//   return (
-//     <div className="transaction-detail">
-//       <div className="transaction-header">
-//         <div className="transaction-summary">
-//           <span className="transaction-user">{transaction.user.username}</span>
-//           <span className="transaction-type"> paid </span>
-//           <span className="transaction-user">{transaction.recipient.username}</span>
-//         </div>
-//
-//         <span className="transaction-amount">{transactionAmount}</span>
-//       </div>
-//       <div className="likes">[likes]</div>
-//       <div className="transaction-memo">{transaction.memo}</div>
-//       <div className="comments">
-//         [comments]
-//       </div>
-//       <div className="add-comment">
-//         <form onSubmit={addComment}>
-//           <input type="text" placeholder="Add a comment..."/>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }

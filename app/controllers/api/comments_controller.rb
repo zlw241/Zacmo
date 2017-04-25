1cbb4a02bf8c811b3e5914de7778cbde3b4f64ce
Zach Ward <zach.l.ward6@gmail.com>
2017-04-24 21:28:23 -0400

    controllers for likes and comments, methods added in comments controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,18 @@
+class Api::CommentsController < ApplicationController
+
+  def create
+    @transaction = Transaction.find(comment_params[:transaction_id])
+    @transaction.comments.new({ user_id: current_user.id, body: comment_params[:body]})
+    # @comment = Comment.new(comment_params)
+    if @transaction.save
+
+      render "api/transactions/show"
+    else
+      render json: @comment.errors.full_messages, status: 401
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :transaction_id)
+  end
+end

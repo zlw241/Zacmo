bb349a9cc81b5b4c677044328e873920d780ad81
Zach Ward <zach.l.ward6@gmail.com>
2017-04-24 21:32:59 -0400

    rails generated/modified files

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424204646) do
+ActiveRecord::Schema.define(version: 20170424234533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",        null: false
+    t.integer  "transaction_id", null: false
+    t.text     "body",           null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "comments", ["transaction_id"], name: "index_comments_on_transaction_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id",    null: false
@@ -25,6 +36,15 @@ ActiveRecord::Schema.define(version: 20170424204646) do
   end
 
   add_index "friendships", ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true, using: :btree
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "transaction_id", null: false
+    t.integer  "user_id",        null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "likes", ["transaction_id", "user_id"], name: "index_likes_on_transaction_id_and_user_id", unique: true, using: :btree
 
   create_table "transactions", force: :cascade do |t|
     t.integer  "user_id",                         null: false

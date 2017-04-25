5528aba33c2b928ed091037dfd1e525ad740a70d
Zach Ward <zach.l.ward6@gmail.com>
2017-04-25 16:33:48 -0400

    add pay icon to side bar header

--- frontend/components/side_bar/side_bar.jsx
+++ frontend/components/side_bar/side_bar.jsx
@@ -1,37 +1,56 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 import { connect } from 'react-redux';
+import Test from '../test/test';
 
 
+const Sidebar = ({currentUser, router}) => {
 
-const Sidebar = ({currentUser}) => {
+  // debugger
+
+  const sideNavClass = (path) => {
+    if (path === router.location.pathname) {
+      return "sidenav-active";
+    } else {
+      return "sidenav-link";
+    }
+  }
+
   if (!currentUser) { return null }
   return (
     <div id="side-bar">
       <div id="sidenav-header">
-        <div id="sidenav-name">{currentUser.first_name} {currentUser.last_name}</div>
-        <div id="sidenav-username">@{currentUser.username}</div>
-        <div id="sidenav-balance">${currentUser.balance}.00</div>
+        <div className="side-nav-user">
+          <div id="sidenav-name">{currentUser.first_name} {currentUser.last_name}</div>
+          <div id="sidenav-username">@{currentUser.username}</div>
+          <div id="sidenav-balance">${currentUser.balance}.00</div>
+        </div>
+        <div className="pay-modal">
+          <Test />
+        </div>
       </div>
       <div id="side-nav">
-        <div className="sidenav-link">
-          <Link to="/home/feed">Feed</Link>
-        </div>
-        <div className="sidenav-link">
-          <Link to="/home/profile">Profile</Link>
-        </div>
-        <div className="sidenav-link">
-          <Link to="/home/friends">Friends</Link>
-        </div>  
-        <div className="sidenav-link">
-          <Link to="/home/settings">Settings</Link>
-        </div>
-        <div className="sidenav-link">
-          <Link to="/home/accounts">Linked Accounts</Link>
-        </div>
-        <div className="sidenav-link">
-          <Link to="/home/notifications">Notifications</Link>
-        </div>
+        <Link className={sideNavClass("/home/feed")} to="/home/feed">
+          Feed
+        </Link>
+        <Link className={sideNavClass("/home/profile")} to="/home/profile">
+          Profile
+        </Link>
+        <Link className={sideNavClass("/home/friends")} to="/home/friends">
+          Friends
+        </Link>
+        <Link className={sideNavClass("/home/settings")} to="/home/settings">
+          Settings
+        </Link>
+        <Link className={sideNavClass("/home/accounts")} to="/home/accounts">
+          Linked Accounts
+        </Link>
+        <Link className={sideNavClass("/home/notifications")} to="/home/notifications">
+          Notifications
+        </Link>
+        <Link className={sideNavClass("/home/test")} to="/home/test">
+          Test
+        </Link>
       </div>
     </div>
   )
@@ -43,4 +62,4 @@ const Sidebar = ({currentUser}) => {
 
 
 // export default connect(mapStateToProps)(Sidebar);
-export default Sidebar;
+export default withRouter(Sidebar);

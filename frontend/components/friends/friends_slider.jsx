e6771291548c50d1ef7c5618b1d406c20b49f686
Zach Ward <zach.l.ward6@gmail.com>
2017-04-28 05:18:28 -0400

    slider to display a user's friends on user's profile

--- frontend/components/friends/friends_slider.jsx
+++ frontend/components/friends/friends_slider.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
-import { link, withRouter } from 'react-router';
+import { Link, withRouter } from 'react-router';
 import moment from 'moment';
 
 
 class FriendSlider extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      imgClass: "friend-slider-img"
+    }
+
+    this.spinTime = this.spinTime.bind(this);
   }
 
-
+  spinTime() {
+    if (this.state.imgClass === "friend-slider-img") {
+      this.setState({
+        imgClass: "spinner friend-slider-img"
+      });
+    } else {
+      this.setState({
+        imgClass: "friend-slider-img"
+      });
+    }
+  }
 
   render() {
     return (
-      <div className="fs-container">
-      <div className="friend-slider scroll">
+
+      <div className="friend-slider scroll" onDoubleClick={this.spinTime}>
         {this.props.friends.map((friend) => (
           <div key={friend.id} className="friend-slider-item scroll-item">
-            <img src={friend.image_url} className="friend-slider-img" />
+            <Link to={`/home/${friend.id}`}>
+              <img src={friend.image_url} className={this.state.imgClass} />
+            </Link>
             <div className="friend-slider-username">
-              {friend.username}
+              <Link to={`/home/${friend.id}`}>{friend.username}</Link>
             </div>
           </div>
         ))}
-      </div>
       </div>
 
     );

206a35f0352e72256839a46ed9fe675661362624
Zach Ward <zach.l.ward6@gmail.com>
2017-05-25 04:36:30 -0400

    comment out redux logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 var middlewares = [thunk];
 
